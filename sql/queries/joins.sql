1459f9ae493d010a19794f2b066da079da7ff827
Kaire Viana <kaireviana20@gmail.com>
2026-09-18 11:30:06 -0300

    que consulta gigante kkkk

--- sql/queries/joins.sql
+++ sql/queries/joins.sql
@@ -9,23 +9,61 @@ SELECT
     visit_id,
     concat(
         first_name, ' ', last_name
-    ) as full_name
+    ) as full_name,
+    state,
+    city,
+    income,
+    score
 from sales.funnel as f 
     left JOIN sales.customers as s 
     on f.customer_id = s.customer_id
 
-
 -- 2. O setor comercial quer visualizar cada registro
 -- do funil junto com as informações do produto.
 
+SELECT
+    visit_id,
+    brand,
+    model,
+    model_year,
+    price
+from sales.funnel as f 
+    left JOIN sales.products as p
+    on f.product_id = p.product_id 
 
 -- 3. O gestor quer visualizar cada registro do funil
 -- junto com a loja correspondente.
 
+SELECT
+    visit_id,
+    store_name
+from sales.funnel as f
+    left JOIN sales.stores as s
+    on f.store_id = s.store_id
 
 -- 4. O gestor quer visualizar os registros do funil
 -- contendo informações do cliente, produto e loja.
 
+SELECT
+    visit_id,
+    concat(
+        first_name, ' ', last_name
+    ) as full_name,
+    state,
+    income,
+    score,
+    brand,
+    model,
+    model_year,
+    price,
+    store_name
+from sales.funnel as f
+    JOIN sales.customers as c
+        on f.customer_id = c.customer_id
+    JOIN sales.products as p
+        on f.product_id = p.product_id
+    JOIN sales.stores as s
+        on f.store_id = s.store_id
 
 -- 5. Quais clientes aparecem no funil de vendas
 -- e quais produtos eles visualizaram?
